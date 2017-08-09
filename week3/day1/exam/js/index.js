369cbb40c034f774d80e895bc383a7c008980549
AimeeLing <lingzi124@yeah.net>
2017-08-09 21:32:37 +0800

    补课数据类型检测

--- week3/day1/exam/js/index.js
+++ week3/day1/exam/js/index.js
@@ -21,4 +21,38 @@ for (var i = 0; i < productData.length; i++) {
     var mallItem = document.getElementById("mallItem"),
         mallList = mallItem.getElementsByTagName("li");
     mallListAry = utils.toArray(mallList);
+    function sortGoods() {
+        var _this = this;
+        var ary = ["data-time", "data-price", "data-hot"];
+        mallListAry.sort(function (cur, next) {
+            var attr = ary[_this.index];
+            var curTime = cur.getAttribute(attr);
+            var nextTime = next.getAttribute(attr);
+            curTime = curTime.replace(/-/g, "");
+            nextTime = nextTime.replace(/-/g, "");
+            return (curTime - nextTime) * _this.n;
+        });
+        var frg = document.createDocumentFragment();
+        for (var i = 0; i < mallListAry.length; i++) {
+            frg.appendChild(mallListAry[i]);
+        }
+        mallItem.appendChild(frg);
+        frg = null;
+    }
+
+    var menu = document.getElementById("menu"),
+        menuLink = menu.getElementsByTagName("a");
+    for (var i = 0; i < menuLink.length; i++) {
+        var curLink = menuLink[i];
+        curLink.n = -1;
+        curLink.index = i;
+        curLink.onclick = function () {
+            for (var j = 0; j < menuLink.length; j++) {
+                var cur = menuLink[j];
+                cur !== this ? cur.n = -1 : null;
+            }
+            this.n *= -1;
+            sortGoods.call(this, this.index);
+        }
+    }
 }();
